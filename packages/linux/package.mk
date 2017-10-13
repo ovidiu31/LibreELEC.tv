87cae7af0e2dcf546103691ef78445faff56b94e
Radostan Riedel <raybuntu@googlemail.com>
2017-10-13 15:05:46 +0200

    New package gcc-linaro-aarch64-linux-gnu
    Odroid_C2: move gcc-linaro-aarch64-elf to projects
    Odroid_C2: move gcc-linaro-arm-eabi to projects

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -75,9 +75,9 @@ PKG_AUTORECONF="no"
 PKG_IS_KERNEL_PKG="yes"
 
 if [ "$TARGET_KERNEL_ARCH" = "arm64" -a "$TARGET_ARCH" = "arm" ]; then
-  PKG_DEPENDS_HOST="$PKG_DEPENDS_HOST gcc-linaro-aarch64-elf:host"
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET gcc-linaro-aarch64-elf:host"
-  TARGET_PREFIX=$TOOLCHAIN/lib/gcc-linaro-aarch64-elf/bin/aarch64-elf-
+  PKG_DEPENDS_HOST="$PKG_DEPENDS_HOST gcc-linaro-aarch64-linux-gnu:host"
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET gcc-linaro-aarch64-linux-gnu:host"
+  TARGET_PREFIX=$TOOLCHAIN/lib/gcc-linaro-aarch64-linux-gnu/bin/aarch64-linux-gnu-
   HEADERS_ARCH=$TARGET_ARCH
 fi
 
